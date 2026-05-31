0a49e2477a74a09ff6135a5f19c453c568baa1bb
quarter2go <jayserchijuni@gmail.com>
2026-05-31 15:56:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+  onSnapshot
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIbHvkDSwYmEAijiRmBwKEBXen-faujTs",
+  authDomain: "chat-for-cuori.firebaseapp.com",
+  projectId: "chat-for-cuori",
+  storageBucket: "chat-for-cuori.firebasestorage.app",
+  messagingSenderId: "278337182213",
+  appId: "1:278337182213:web:f2b43dcda98e2a2c421a6c",
+  measurementId: "G-8LCVNCQF6R"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const modal = document.getElementById("modal");
+const addBtn = document.getElementById("addBtn");
+
+const publishBtn =
+document.getElementById("publishBtn");
+
+addBtn.onclick = () => {
+  modal.classList.remove("hidden");
+};
+
+let profiles = [];
+let current = 0;
+
+function showProfile() {
+
+  if(profiles.length === 0){
+    return;
+  }
+
+  const p = profiles[current];
+
+  document.getElementById("name")
+  .textContent = p.name;
+
+  document.getElementById("major")
+  .textContent = p.major;
+
+  document.getElementById("about")
+  .textContent = p.about;
+
+  document.getElementById("looking")
+  .textContent =
+  "Looking For: " + p.looking;
+}
+
+publishBtn.onclick = async () => {
+
+  const name =
+  document.getElementById("nameInput")
+  .value.trim();
+
+  if(!name) return;
+
+  await setDoc(
+    doc(db,"profiles",name),
+    {
+      name,
+      major:
+      document.getElementById("majorInput")
+      .value,
+
+      about:
+      document.getElementById("aboutInput")
+      .value,
+
+      looking:
+      document.getElementById("lookingInput")
+      .value
+    }
+  );
+
+  modal.classList.add("hidden");
+};
+
+onSnapshot(
+  collection(db,"profiles"),
+  (snapshot)=>{
+
+    profiles = [];
+
+    snapshot.forEach(doc=>{
+      profiles.push(doc.data());
+    });
+
+    if(current >= profiles.length){
+      current = 0;
+    }
+
+    showProfile();
+  }
+);
+
+document
+.getElementById("nextBtn")
+.onclick = ()=>{
+
+  if(profiles.length===0) return;
+
+  current++;
+
+  if(current >= profiles.length){
+    current = 0;
+  }
+
+  showProfile();
+};
+
+document
+.getElementById("prevBtn")
+.onclick = ()=>{
+
+  if(profiles.length===0) return;
+
+  current--;
+
+  if(current < 0){
+    current = profiles.length - 1;
+  }
+
+  showProfile();
+};
